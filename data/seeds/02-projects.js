a6af2740b56be306af68014c806c81538fa34f41
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-24 13:30:15 -0500

    all seeds

--- data/seeds/02-projects.js
+++ data/seeds/02-projects.js
@@ -1,13 +1,8 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('projects').del()
-    .then(function () {
-      // Inserts seed entries
       return knex('projects').insert([
         {id: 1, name: 'learn to code'},
         {id: 2, name: 'practice singing'},
         {id: 3, name: 'work meeting'}
       ]);
-    });
 };
