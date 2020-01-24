7d03b080f114ee06704462eac8e91ddd1e62a38f
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-24 14:17:23 -0500

    attempt to get tasks

--- router/project-model.js
+++ router/project-model.js
@@ -1,9 +1,22 @@
 const db = require("../data/db-config.js");
 
 module.exports = {
-    getProjects
+    getProjects,
+    getResources,
+    getTasks
 }
 
 function getProjects() {
     return db.select('*').from('projects');
+}
+
+function getResources() {
+    return db.select('*').from('resources');
+}
+
+function getTasks() {
+    return db('tasks')
+    .join('projects', 'projects.id', 'tasks.project_id')
+    .select('projects.name', 'projects.description', 'tasks')
+    .where('tasks.project_id', 'projects.id');
 }
--- router/project-router.js
+++ router/project-router.js
@@ -14,4 +14,24 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/resources", (req, res) => {
+    Projects.getResources()
+    .then(resources => {
+        res.json(resources);
+    })
+    .catch(err => {
+        res.status(500).json({message: "Failed to get resources"})
+    });
+});
+
+router.get("/tasks", (req, res) => {
+    Projects.getTasks()
+    .then(tasks => {
+        res.json(tasks);
+    })
+    .catch(err => {
+        res.status(500).json({message: "Failed to get tasks"})
+    });
+});
+
 module.exports = router;
